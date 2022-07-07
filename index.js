47e25492a68752d43a425a22d76762e39d407316
xauvk <xauvkubalexanderyang@live.com>
2022-07-06 20:08:56 -0400

    commit tree 303785dfd68425ae5c88348312cf4ad27929f2f1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const newArray = []
+  for (let i = 0; i < names.length; i++) {
+    newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  }
+  return newArray
+}
+
+function countDown(num) {
+  for (let i = num; i > -1; i--) {
+    console.log(i)
+  }
+}
